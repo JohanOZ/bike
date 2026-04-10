ad1e85fdc562f6c2510e1a47d63ae9bdcae8f253
Johan Olea <johanolea@hotmail.com>
2026-04-10 09:25:39 -0600

    Initial Commit

---
+++ Bike/BikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeApp.swift
+//  Bike
+//
+//  Created by Johan Olea on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
